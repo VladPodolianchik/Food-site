bf8437caddae7a686ec57d0f4a5608be950b94bc
Vlad <Somemail@gmail.com>
2023-01-21 17:54:13 +0300

    used little regular exp and made function for repeating code

--- js/script.js
+++ js/script.js
@@ -459,11 +459,28 @@ window.addEventListener('DOMContentLoaded', () =>{
         dots.push(dot);
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }    
+
+    function checkSlidesZero(index, elem) {
+        if (index < 10) {
+            elem.textContent =`0${index}`;
+        } else {
+            elem.textContent = index;
+        }
+    }
+
+    function dotsOpacity(elems, index) {
+        elems.forEach(dot => dot.style.opacity = '.5');
+        elems[index - 1].style.opacity = 1;
+    }
+
     next.addEventListener('click', () => {
-        if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset === deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -474,21 +491,16 @@ window.addEventListener('DOMContentLoaded', () =>{
             slideIndex++;
         }
 
-        if (slideIndex < 10) {
-            current.textContent =`0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        checkSlidesZero(slideIndex, current);
 
-        dots.forEach(dot => dot.style.opacity = '.5');
-        dots[slideIndex - 1].style.opacity = 1;
+        dotsOpacity(dots, slideIndex);
     })
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -499,14 +511,9 @@ window.addEventListener('DOMContentLoaded', () =>{
             slideIndex--;
         }
 
-        if (slideIndex < 10) {
-            current.textContent =`0${slideIndex}`;
-        } else {
-            current.textContent = slideIndex;
-        }
+        checkSlidesZero(slideIndex, current);
 
-        dots.forEach(dot => dot.style.opacity = '.5');
-        dots[slideIndex - 1].style.opacity = 1;
+        dotsOpacity(dots, slideIndex);
     });
 
 
@@ -515,18 +522,13 @@ window.addEventListener('DOMContentLoaded', () =>{
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = deleteNotDigits(width) * (slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
-            if (slideIndex < 10) {
-                current.textContent =`0${slideIndex}`;
-            } else {
-                current.textContent = slideIndex;
-            }
+            checkSlidesZero(slideIndex, current);
 
-            dots.forEach(dot => dot.style.opacity = '.5');
-            dots[slideIndex - 1].style.opacity = 1;
+            dotsOpacity(dots, slideIndex);
         })
     })
 
